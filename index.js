ffa27abd53324f6e260d128a1871456ca81a1c2c
Joe Sullivan <its.joe.sullivan@gmail.com>
2013-10-02 19:59:58 -0400

    retooling with new api

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 define({
+
   name: "echo",
-  "expr": /^echo (.*)/,
-  "fn": function(argv) {
-    this.stdout.write(argv.slice(1).join(' ') );
-    this.trigger('exit');
+  
+  "fn": function(text) {
+    this.stdout.write(text);
     this.exit();
+  },
+
+  parse: function(argv) {
+    return argv.replace(/^echo /, '');
   }
+
 });
